9581f24d58afb6a82efa011f1e69bb1b31292e87
Andrey Mukamolov <fobo66@protonmail.com>
2019-11-03 22:08:49 +0300

    Initial Commit

---
+++ valiutchik/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  valiutchik
+//
+//  Created by Andrei Mukamolau on 11/3/19.
+//  Copyright © 2019 Andrei Mukamolau. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
