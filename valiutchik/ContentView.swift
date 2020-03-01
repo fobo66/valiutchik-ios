f942c72d62c39c540c09bf3e0902a673111e3f7f
Andrey Mukamolov <fobo66@protonmail.com>
2020-03-01 10:21:12 +0300

    Use localized string in toolbar

--- valiutchik/ContentView.swift
+++ valiutchik/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
             }
             .padding()
             .navigationBarTitle(
-                Text(title)
+                title
             )
         }
     }
