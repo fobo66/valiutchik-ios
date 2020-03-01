a32097e8b8ebb6335aa1a114da10b84d566e1164
Andrey Mukamolov <fobo66@protonmail.com>
2020-03-02 01:12:23 +0300

    Added dependencies

---
+++ Pods/Target Support Files/Pods-valiutchik-valiutchikUITests/Pods-valiutchik-valiutchikUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_valiutchik_valiutchikUITests : NSObject
+@end
+@implementation PodsDummy_Pods_valiutchik_valiutchikUITests
+@end
---
+++ Pods/Target Support Files/Pods-valiutchikTests/Pods-valiutchikTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_valiutchikTests : NSObject
+@end
+@implementation PodsDummy_Pods_valiutchikTests
+@end
