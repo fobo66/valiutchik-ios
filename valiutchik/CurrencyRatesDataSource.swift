f8d6d9012ac94f42aad54960f72fdfea25fb1187
Andrey Mukamolov <amv@bamboogroup.eu>
2020-03-10 17:16:19 +0300

    Populated Myfin data source

--- valiutchik/CurrencyRatesDataSource.swift
+++ valiutchik/CurrencyRatesDataSource.swift
@@ -5,14 +5,34 @@
 //  Created by Andrei Mukamolau on 3/5/20.
 //
 
-import Foundation
+import Alamofire
 
 protocol CurrencyRatesDataSource {
-    func fetchCourses(city: String) -> [Currency]
+    func fetchCourses(city: String)
 }
 
 class MyfinDataSource: CurrencyRatesDataSource {
-    func fetchCourses(city: String) -> [Currency] {
-        return []
+    
+    private let citiesMap = [
+        "Минск": "1",
+        "Витебск": "2",
+        "Гомель": "3",
+        "Гродно": "4",
+        "Брест": "5",
+        "Могилёв": "6"
+    ]
+    
+    func fetchCourses(city: String) {
+        
+        let url = resolveUrl(city: city)
+        request(url).responseXMLArray(queue: DispatchQueue.global(), keyPath: "root") { (response: DataResponse<[Currency]>) in
+            let currencies = response.result.value
+        }
+    }
+    
+    private func resolveUrl(city: String) -> String {
+        let cityEndpoint = citiesMap[city] ?? "1"
+        
+        return "https://admin.myfin.by/outer/authXml/" + cityEndpoint
     }
 }
