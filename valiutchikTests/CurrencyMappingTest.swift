ef7e69cadd2ae47cfb9adec5fff6f39055204674
Andrey Mukamolov <amv@bamboogroup.eu>
2020-05-07 11:00:28 +0300

    Added initial test for XML mapping

---
+++ valiutchikTests/CurrencyMappingTest.swift
@@ -0,0 +1,44 @@
+//
+//  CurrencyMappingTest.swift
+//  valiutchikTests
+//
+//  Created by Andrei Mukamolau on 5/7/20.
+//  Copyright © 2020 Andrei Mukamolau. All rights reserved.
+//
+
+import XCTest
+import XMLMapper
+
+class CurrencyMappingTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testFileMapping() throws {
+        let bundle = Bundle(for: type(of: self))
+
+        guard let url = bundle.url(forResource: "data", withExtension: "xml") else {
+            XCTFail("Missing file with test data: data.xml")
+            return
+        }
+        
+        let xml = try String(contentsOf: url)
+        
+        let parsedXml = XMLMapper<Currency>().map(XMLString: xml)
+        
+        XCTAssert(parsedXml != nil)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
