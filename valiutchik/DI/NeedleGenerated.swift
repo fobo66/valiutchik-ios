9a1785cf9365d6e1e41a12e946cfe23f415b69b5
Andrey Mukamolov <amv@bamboogroup.eu>
2021-07-13 11:27:39 +0300

    Generated Needle class

---
+++ valiutchik/DI/NeedleGenerated.swift
@@ -0,0 +1,14 @@
+
+
+
+
+// swiftlint:disable unused_declaration
+private let needleDependenciesHash : String? = nil
+
+// MARK: - Registration
+
+public func registerProviderFactories() {
+    
+}
+
+// MARK: - Providers
