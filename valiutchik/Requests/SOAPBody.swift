cefffa25a2654ec73d0992108938ff7952c6d068
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2024-03-16 16:15:47 +0200

    Added XmlMapperRequests classes

---
+++ valiutchik/Requests/SOAPBody.swift
@@ -0,0 +1,28 @@
+//
+//  SOAPBody.swift
+//  XMLMapper
+//
+//  Created by Giorgos Charitakis on 02/10/2017.
+//
+
+import Foundation
+import XMLMapper
+
+public class SOAPBody: XMLMappable {
+    public var nodeName: String!
+    
+    var soapMessage: SOAPMessage?
+    
+    private var soapAction: String?
+    
+    init(soapMessage: SOAPMessage) {
+        self.soapAction = soapMessage.soapAction
+        self.soapMessage = soapMessage
+    }
+    
+    required public init?(map: XMLMap) {}
+    
+    public func mapping(map: XMLMap) {
+        soapMessage <- map["m:\(soapAction ?? "")"]
+    }
+}
---
+++ valiutchik/Requests/SOAPMessage.swift
@@ -0,0 +1,27 @@
+//
+//  SOAPMessage.swift
+//  XMLMapper
+//
+//  Created by Giorgos Charitakis on 02/10/2017.
+//
+
+import Foundation
+import XMLMapper
+
+open class SOAPMessage: XMLMappable {
+    public var nodeName: String!
+    
+    private var xmlnsMessage: String?
+    var soapAction: String?
+    
+    public init(soapAction: String, nameSpace: String) {
+        self.soapAction = soapAction
+        self.xmlnsMessage = nameSpace
+    }
+    
+    required public init?(map: XMLMap) {}
+    
+    open func mapping(map: XMLMap) {
+        xmlnsMessage <- map.attributes["xmlns:m"]
+    }
+}
