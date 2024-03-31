1278da24a85d3eabf0cbbe6e1a0e25928b4c605b
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2024-03-31 13:55:23 +0300

    Added initial Needle root component

---
+++ valiutchik/DI/RootComponent.swift
@@ -0,0 +1,15 @@
+//
+//  RootComponent.swift
+//  valiutchik
+//
+//  Created by Andrei Mukamolau on 31.03.24.
+//  Copyright © 2024 Andrei Mukamolau. All rights reserved.
+//
+import SwiftUI
+import NeedleFoundation
+
+class RootComponent : BootstrapComponent {
+    var userDefaults: UserDefaults {
+        UserDefaults.standard
+      }
+}
