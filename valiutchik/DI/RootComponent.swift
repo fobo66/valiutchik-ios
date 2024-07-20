abd3dd1a51e00cb9ff33f5b441e39dea09563fc9
Andrey Mukamolov <4471821+fobo66@users.noreply.github.com>
2024-07-20 17:24:36 +0300

    More Needle deps

--- valiutchik/DI/RootComponent.swift
+++ valiutchik/DI/RootComponent.swift
@@ -5,11 +5,22 @@
 //  Created by Andrei Mukamolau on 31.03.24.
 //  Copyright © 2024 Andrei Mukamolau. All rights reserved.
 //
-import SwiftUI
 import NeedleFoundation
+import SwiftUI
+import TomTomSDKReverseGeocoder
+import TomTomSDKReverseGeocoderOnline
 
-class RootComponent : BootstrapComponent {
+class RootComponent: BootstrapComponent {
     var userDefaults: UserDefaults {
         UserDefaults.standard
-      }
+    }
+
+    var reverseGeocoder: TomTomSDKReverseGeocoder.ReverseGeocoder? {
+        try? OnlineReverseGeocoderFactory.create(apiKey: userDefaults.string(forKey: "TTTomTomAccessToken") ?? ""
+        )
+    }
+
+    var preferenceDataSource: PreferenceDataSourceProtocol {
+        PreferenceDataSource(userDefaults)
+    }
 }
